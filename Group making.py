1950a6be27644d5ce77c7f1127a5475c3c977dd1
Muriel Grolaux <muriel_grolaux@hotmail.fr>
2026-05-22 14:45:17 +0200

    Code.py

---
+++ Group making.py
@@ -0,0 +1,21 @@
+from random import randint
+# list of student
+students = ["Camilla","Mary","Muriel","Myriam","Sibylle","Kaya","Imane B.","Parita","Elena","Julie","Imane Z."]
+
+# making groups
+group_of = 2
+x_group = 5
+
+
+for i in range (x_group) :
+    
+    for k in range (group_of):
+        rdm = randint(0,len(students) -1)
+        print(students[rdm])
+        students.pop(rdm)
+
+    print()
+
+
+
+
